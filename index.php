b6fdd2a0b70c88bf0a18b716f4c338e4e9d84702
noushiyo <122967507+noushiyo@users.noreply.github.com>
2024-02-17 03:46:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<title>poc-application for jQuery CVE</title>
+<meta name="description" content="Our first page">
+<meta name="keywords" content="html tutorial template">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+<script>
+function test(){
+  testvar = document.getElementById('id').innerHTML;
+    $('#div').html(testvar);
+}
+ 
+</script>
+
+</head>
+
+<body>
+
+<form>
+    <label for="textBox">Enter Text:</label>
+    <input type="text" name="value" id="textBox">
+	<button onclick="test()">click</button>
+</form>
+
+
+<xmp id="id">
+<img alt="<x" title="<?php echo($_GET['value']); ?>">
+</xmp>
+<div id="div"></div>
+
+</body>
+</html>
